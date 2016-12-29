645ebadb4288163743c634ba3f70da5e659a90e8
三谷 哲也 <mitani0328@hotmail.co.jp>
2016-12-29 10:26:02 +0900

    Initial Commit

---
+++ SampleQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleQuiz
+//
+//  Created by 三谷 哲也 on 2016/12/29.
+//  Copyright © 2016年 Tetsuya Mitani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
