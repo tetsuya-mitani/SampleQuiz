cde9719eacfac747621fada89b735fbb5ec68c45
三谷 哲也 <mitani0328@hotmail.co.jp>
2016-12-31 00:31:16 +0900

    サウンドとバイブレーションのサンプル実装

--- SampleQuiz/ViewController.swift
+++ SampleQuiz/ViewController.swift
@@ -7,9 +7,32 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func tapSystemSound(_ sender: Any) {
+        AudioServicesPlaySystemSoundWithCompletion(1000) {
+            //サウンドが鳴り終わった後の処理
+        }
+        
+    }
+    @IBAction func tapCustomSound(_ sender: Any) {
+        let soundUrl = Bundle.main.url(forResource: "small-bell02", withExtension: "mp3")
+        
+        var soundId: SystemSoundID = 0
+        AudioServicesCreateSystemSoundID(soundUrl as! CFURL, &soundId)
+        AudioServicesPlaySystemSoundWithCompletion(soundId) {
+            //サウンドが鳴り終わった後の処理
+        }
+    }
+    
+    @IBAction func tapVibration(_ sender: Any) {
+        AudioServicesPlaySystemSoundWithCompletion(kSystemSoundID_Vibrate) {
+            //サウンドが鳴り終わった後の処理
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
